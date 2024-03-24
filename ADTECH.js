67b4ff2b6183ab7b9f2f8a2bafe72c2b50654da7
Glory_Kuria <kuriaglory@gmail.com>
2024-03-24 21:01:46 +0300

    add new file

--- ADTECH.js
+++ ADTECH.js
@@ -8,18 +8,18 @@
 //     }
 // }
 
-var id = null;
-function myMove(){
-    var elem = document.getElementById("myAnimation");
-    var pos = 0;
-    id = setInterval(frame,10);
-    function frame() {
-if (pos ==350) {
-    clearInterval(id);
-}else{
-    pos++;
-elem.style.top = pos+ 'px';
-elem.style.left = pos+ 'px'
-}
-    }
-}
+// var id = null;
+// function myMove(){
+//     var elem = document.getElementById("myAnimation");
+//     var pos = 0;
+//     id = setInterval(frame,10);
+//     function frame() {
+// if (pos ==350) {
+//     clearInterval(id);
+// }else{
+//     pos++;
+// elem.style.top = pos+ 'px';
+// elem.style.left = pos+ 'px'
+// }
+//     }
+//  }
